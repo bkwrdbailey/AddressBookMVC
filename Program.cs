0bd05e55c09905a66a2af8602fb1cb11d66914e1
bkwrdbailey <Baileybarton77@yahoo.com>
2022-02-18 08:28:39 -0600

    Small Progress
    Working on adding PostgreSQL

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
